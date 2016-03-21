4ce8b28d95849b19a83c7c4bbc022de557b7de6c
Shahab <tausiq19@gmail.com>
2016-03-21 15:07:02 +0600

    minor changes

--- app/src/main/java/com/genericslab/droidapp/App.java
+++ app/src/main/java/com/genericslab/droidapp/App.java
@@ -1,10 +1,24 @@
 package com.genericslab.droidapp;
 
 import com.genericslab.droidplate.app.CoreApplication;
+import com.genericslab.droidplate.log.Tracer;
 
 /**
  * Created by shahab on 12/24/15.
  */
 // Config: Extend from @CoreApplication and also set the <application name=""> tag
 public class App extends CoreApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Tracer.i("Application Id: " + BuildConfig.APPLICATION_ID
+                + " || Version code: " + BuildConfig.VERSION_CODE
+                + " || Version name: " + BuildConfig.VERSION_NAME
+                + " || Git Sha: " + com.genericslab.droidplate.BuildConfig.GIT_SHA
+                + " || Build time:  " + com.genericslab.droidplate.BuildConfig.BUILD_TIME
+                + " || Build flavor: " + BuildConfig.FLAVOR
+                + " || Build type: " + BuildConfig.BUILD_TYPE);
+    }
 }
