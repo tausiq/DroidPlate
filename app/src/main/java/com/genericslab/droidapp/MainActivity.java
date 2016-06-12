73a9388a720f642d6b735c46863e697be97d177b
Shahab <tausiq19@gmail.com>
2016-06-12 22:22:09 +0600

    icon update and introduces splash image

--- app/src/main/java/com/genericslab/droidapp/MainActivity.java
+++ app/src/main/java/com/genericslab/droidapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.genericslab.droidapp;
 
+import android.os.Bundle;
+
 import com.genericslab.droidplate.CoreActivity;
 import com.genericslab.droidplate.config.Config;
 import com.genericslab.droidplate.ui.drawer.DrawerActivity_;
@@ -12,6 +14,12 @@ import org.androidannotations.annotations.Trace;
 public class MainActivity extends CoreActivity {
 
     protected final String TRACE_TAG = Config.TRACE_TAG + "MainActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+    }
 
     @Trace(tag = TRACE_TAG)
     @AfterViews
